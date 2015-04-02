aba9cc43a58f566ffd4b14af4eae3419c7ba6a2b
jenniferpham <j.pham87@gmail.com>
2015-04-02 11:52:56 -0700

    added routes in nav bar

--- client/app/app.js
+++ client/app/app.js
@@ -9,6 +9,35 @@ angular.module('castCentricAppApp', [
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
+          .when('/', {
+            templateUrl: 'app/main/main.html',
+            controller: 'MainCtrl'
+          })
+
+          .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'AuthController'
+          })
+          .when('/register', {
+            templateUrl: 'views/register.html',
+            controller: 'AuthController'
+          })
+          .when('/FindJob', {
+            templateUrl: 'app/FindJob/FindJob.html',
+            controller: 'FindJobCtrl'
+          })
+          .when('/FindTalent', {
+            templateUrl: 'app/FindTalent/FindTalent.html',
+            controller: 'FindTalentCtrl'
+          })
+          .when('/FindAgent', {
+            templateUrl: 'app/FindAgent/FindAgent.html',
+            controller: 'FindAgentCtrl'
+          })
+      .when('/Testimonials', {
+        templateUrl: 'app/Testimonials/Testimonials.html',
+        controller: 'TestimonialsCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -5,7 +5,23 @@ angular.module('castCentricAppApp')
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
-    }];
+    },
+      {
+        'title': 'Find Audition',
+        'link': '/FindJob'
+      },
+      {
+        'title': 'Find Talent',
+        'link': '/FindTalent'
+      },
+      {
+        'title': 'Find Agent',
+        'link': '/FindAgent'
+      },
+      {
+        'title': 'Testimonials',
+        'link': '/Testimonials'
+      }];
 
     $scope.isCollapsed = true;
 
