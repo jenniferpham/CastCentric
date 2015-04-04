59f10c9e8192c25373201b040f585fd0582ba217
diennguyen90 <dienM.Nguyen90@gmail.com>
2015-04-04 00:26:06 -0700

    added more examples to see if params are working and it is!

--- server/config/actor.data.js
+++ server/config/actor.data.js
@@ -36,5 +36,39 @@ Actor.find({}).remove(function(){
 		Year: 2014
 	},
 	OpUrl: 'Facebook.com'
-	})
+	},{
+	Name: 'Sally Mo',
+	Email:'SallyMo@gmail.com',
+	ProfileImage: 'https://s-media-cache-ak0.pinimg.com/236x/6e/4b/11/6e4b11e8a5928e7acaa7fb9effac8043.jpg',
+	Role:'actor',
+	Location: 'Santa Monica, Ca',
+	WorkHistory: {
+		Title: 'Avengers',
+		RoleType: 'Lead Role',
+		Director: 'John Whedon',
+		Company: 'Disney'
+	},
+	PhysicalAttribute: {
+		Gender: 'Female',
+		AgeRange:{ 
+			Start: 24,
+		    End: 35
+		},
+		Height: "5'10",
+		Build: 'Built',
+		Hair: 'Comb Over',
+		Eyes: 'Green',
+		Ethnicity: 'Spanish'
+	},
+	EducationTraining:{
+		School: 'Santa Monica College',
+		Teacher: '',
+		Degree: 'Theater',
+		Location: 'Santa Monica,CA',
+		Year: 2014
+	},
+	OpUrl: 'Facebook.com'
+
+	}
+	)
 });
