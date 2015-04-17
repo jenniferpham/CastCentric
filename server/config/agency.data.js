878f8a98e38da7b07fb09081315323825ceaf00a
sdadabhoy <Sdadabhoy.sdadabhoy@gmail.com>
2015-04-17 12:45:44 -0700

    find agent file updated with filter

--- server/config/agency.data.js
+++ server/config/agency.data.js
@@ -1,19 +1,21 @@
-'user strict';
+'use strict';
 
 var Agency = require('../api/agency/agency.model.js')
 
 Agency.find({}).remove(function(){
 	Agency.create({
-		Company: 'SomeAgency',
-		Email: 'Tim@SomeAgency.com',
-		Location: 'Beverly Hills, CA',
-<<<<<<< HEAD
+		Company: 'United Talent Agency',
+		Email: 'Tim@UTA.com',
+		Location: 'Los Angeles',
 		Description: 'Top notch Agency',
     Image: "http://extras.mnginteractive.com/live/media/site241/2012/0402/20120402_031313_180048v2-max-250x250.png"
-	})
-=======
-		Description: 'Top notch Agency'
-	}
-	)
->>>>>>> 89238208a73384e6305588c7ed146ef8357124ef
+	},
+    {
+      Company: 'Stewart Talent Group',
+      Email: 'AriGold@sta.com',
+      Location: 'Chicago',
+      Description: "Chicago's premier full service agency",
+      Image: "http://www.stewarttalent.com/assets/index_title.png"
+    })
+
 });
