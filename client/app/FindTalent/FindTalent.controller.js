9a33927cb0a5d6f7707878970fa54b525ef50d9c
dienm.nguyen90@gmail.com <dienM.Nguyen90@gmail.com>
2015-04-23 13:47:52 -0700

    ngshow on add button

--- client/app/FindTalent/FindTalent.controller.js
+++ client/app/FindTalent/FindTalent.controller.js
@@ -1,10 +1,12 @@
 'use strict';
 
 angular.module('castCentricApp')
-  .controller('FindTalentCtrl', function ($scope,$http) {
+  .controller('FindTalentCtrl', function ($scope,$http,Auth) {
     $scope.actors = [];
 
     $http.get('/api/actors').success(function(actors){
     	$scope.actors = actors
     })
+
+    $scope.isLoggedIn=Auth.isLoggedIn;
   });
